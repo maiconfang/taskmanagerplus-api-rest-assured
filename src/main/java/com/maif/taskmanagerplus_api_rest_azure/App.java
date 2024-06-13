6fe418794ad62362c5ad8eed9a248c7737da3f87
Maicon <maicon.fang@gmail.com>
2024-06-13 10:04:42 -0300

    first commit

---
+++ src/main/java/com/maif/taskmanagerplus_api_rest_azure/App.java
@@ -0,0 +1,13 @@
+package com.maif.taskmanagerplus_api_rest_azure;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/com/maif/taskmanagerplus_api_rest_azure/CalculatorTest.java
@@ -0,0 +1,5 @@
+package com.maif.taskmanagerplus_api_rest_azure;
+
+public class CalculatorTest {
+
+}
