1443158956b4c2ac71167f1cc0da9bcfc439fc41
Maicon <maicon.fang@gmail.com>
2024-06-18 11:51:18 -0300

    Created document of the class TestUtil

--- src/main/java/com/maif/taskmanagerplus_api_rest_azure/util/TestUtil.java
+++ src/main/java/com/maif/taskmanagerplus_api_rest_azure/util/TestUtil.java
@@ -5,6 +5,17 @@ import com.maif.taskmanagerplus_api_rest_azure.auth.AuthUtil;
 
 import io.restassured.specification.RequestSpecification;
 
+/**
+ * Adds authorization token and Content-Type header to the given RequestSpecification.
+ * The authorization token is fetched using AuthUtil.getAuthToken().
+ *
+ * @param requestSpec The RequestSpecification to which headers will be added.
+ * @return The updated RequestSpecification with Authorization and Content-Type headers.
+ * 
+ * Author: Maicon Fang
+ * Date: 2024-06-18
+ * 
+ */
 public class TestUtil {
 
     public static RequestSpecification addTokenHeader(RequestSpecification requestSpec) {
